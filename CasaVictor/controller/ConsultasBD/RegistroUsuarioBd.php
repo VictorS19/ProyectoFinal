c901e89ae08150ebbc2184a35a3b5c9442ab6935
VictorS19 <visaenz99@gmail.com>
2023-05-08 16:50:47 +0200

    victor 0805

--- CasaVictor/controller/ConsultasBD/RegistroUsuarioBd.php
+++ CasaVictor/controller/ConsultasBD/RegistroUsuarioBd.php
@@ -6,37 +6,17 @@
     {
 
         //hacer dos consultas, con una verificamos que el dni no esta repetido (usuario ya registrado). Con la otra hacer el insert
-        $sql = 'insert into usuario values (:correo,:pass,:rol)';
-        $sql2 = 'select dni from usuarios dni = '.$dni;
-        $sql3 = 'insert into pasajero values (:nombre,:apellido,:dni,:idUsr,:fNac)';
+        $sql1 = 'select dni from usuarios where dni = '.$dni;
+        $sql2 = 'insert into usuarios values (:nombre,:apellidos,:dni,:fNac,:correo,:pass)';
 
         require_once '/../appConf/conexionBd.inc';
-        
+    
         try {
-            //añadir a la tabla usuario
+            //consultar si el dni del usuario ya existe
             $con = (new \ConexionBd())->getConexion();
-            $stn = $con->prepare($sql);
-            $stn->bindValue(":correo", $correo);
-            $stn->bindValue(":pass", $pass);
-            $stn->bindValue(":rol", 'pasajero');
+            $stn = $con->prepare($sql1);
             $stn->execute();
-           
-        } catch (\PDOException $ex) {
-            throw $ex;
-        } finally {
-            unset($stn);
-            unset($con);
-        }
-        $idUsuario = '';
-        try {
-            //consultar la id de usuario
-            $con = (new \ConexionBd())->getConexion();
-            $stn = $con->prepare($sql2);
-            $stn->bindValue(":ecorreo", $correo);
-            $stn->bindValue(":pwd", $pass);
-            $stn->execute();
-            $idUsuario = $stn->fetchAll(\PDO::FETCH_ASSOC);
-            $idUsuario = $idUsuario[0]['idUsers'];
+            $result = $stn->fetchAll(\PDO::FETCH_ASSOC);
         } catch (\PDOException $ex) {
             throw $ex;
         } finally {
@@ -44,6 +24,10 @@
             unset($con);
         }
 
+        if(empty($result[0])){
+
+
+        
 
         try {
             //añadir a la tabla pasajero
@@ -54,6 +38,8 @@
             $stn->bindValue(":dni", $dni);
             $stn->bindValue(":idUsr", $idUsuario);
             $stn->bindValue(":fNac", $fechaNac);
+            $stn->bindValue(":correo", $correo);
+            $stn->bindValue(":pass", $pass);
             $stn->execute();
            
         } catch (\PDOException $ex) {
@@ -62,5 +48,8 @@
             unset($stn);
             unset($con);
         }
-    
+    }else{
+        //TODO mostrar error en el registro
+
+    }
     }
